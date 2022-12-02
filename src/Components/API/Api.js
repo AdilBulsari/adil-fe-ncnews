62f04cbe47a8f7d82cbc746a0acdcf2b4be8d593
AdilBulsari <adilbulsari@gmail.com>
2022-12-02 23:15:50 +0000

    updated api url

--- src/Components/API/Api.js
+++ src/Components/API/Api.js
@@ -1,44 +1,33 @@
 import axios from 'axios'
-
+const url = 'https://adil-nc-news.cyclic.app'
 export function getArticlesForTopics(topic_name) {
     return axios
-        .get(
-            `https://adil-nc-news.herokuapp.com/api/articles?filter_by=${topic_name}`
-        )
+        .get(`${url}/api/articles?filter_by=${topic_name}`)
         .then((res) => {
             return res.data
         })
 }
 export const deleteComment = (comment_id) => {
-    return axios
-        .delete(`https://adil-nc-news.herokuapp.com/api/comments/${comment_id}`)
-        .catch((err) => {
-            console.log(err)
-        })
+    return axios.delete(`${url}/api/comments/${comment_id}`).catch((err) => {
+        console.log(err)
+    })
 }
 
 export function getAllTopics() {
-    return axios
-        .get('https://adil-nc-news.herokuapp.com/api/topics')
-        .then((res) => {
-            return res.data
-        })
+    return axios.get(`${url}/api/topics`).then((res) => {
+        return res.data
+    })
 }
 
 export function getArticleById(article_id) {
-    return axios
-        .get(`https://adil-nc-news.herokuapp.com/api/articles/${article_id}`)
-        .then((res) => {
-            return res.data
-        })
+    return axios.get(`${url}/api/articles/${article_id}`).then((res) => {
+        return res.data
+    })
 }
 
 export function patchVoteById(article_id, vote_count) {
     return axios
-        .patch(
-            `https://adil-nc-news.herokuapp.com/api/articles/${article_id}`,
-            { inc_votes: vote_count }
-        )
+        .patch(`${url}/api/articles/${article_id}`, { inc_votes: vote_count })
         .then((res) => {
             return res.data
         })
@@ -46,9 +35,7 @@ export function patchVoteById(article_id, vote_count) {
 
 export function getCommentsByArticleId(article_id) {
     return axios
-        .get(
-            `https://adil-nc-news.herokuapp.com/api/articles/${article_id}/comments`
-        )
+        .get(`${url}/api/articles/${article_id}/comments`)
         .then((res) => {
             console.log(res.data)
             return res.data
@@ -56,16 +43,11 @@ export function getCommentsByArticleId(article_id) {
 }
 
 export function postCommentByArticleId(article_id, body) {
-    return axios.post(
-        `https://adil-nc-news.herokuapp.com/api/articles/${article_id}/comments`,
-        body
-    )
+    return axios.post(`${url}/api/articles/${article_id}/comments`, body)
 }
 
 export function sortByDate(type) {
-    return axios
-        .get(`https://adil-nc-news.herokuapp.com/api/articles/?sort_by=${type}`)
-        .then((res) => {
-            return res.data
-        })
+    return axios.get(`${url}/api/articles/?sort_by=${type}`).then((res) => {
+        return res.data
+    })
 }
