13337ae447ca2fef60bb9faafd687472de94eb96
AdilBulsari <adilbulsari@gmail.com>
2022-12-02 23:26:59 +0000

    optimized code

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import styles from '../Home/Home.module.css'
 function Home() {
     return (
         <div className={styles.home}>
-            <h1 className={styles.header_home}>Welcome to NC-NEWS</h1>
+            <h1 className={styles.header_home}>Welcome to Adil's NC-NEWS</h1>
         </div>
     )
 }
