b7846800548b52bb8b5f223e3ca1db1f13c6d3b2
AdilBulsari <adilbulsari@gmail.com>
2023-04-03 12:29:32 +0100

    changed voting component and styling

--- src/Components/Topics/Vote.jsx
+++ src/Components/Topics/Vote.jsx
@@ -1,45 +1,20 @@
 import { useState } from 'react'
 import { patchVoteById } from '../API/Api'
-import Button from './Button'
 import styles from './Vote.module.css'
-import likeicon from '../Tools/LikeSVG/likeicon.png'
-import dislikeicon from '../Tools/LikeSVG/dislikeicon.png'
+import upVote from '../../assets/bottom-angle-arrow-bold-icon.svg'
+import downVote from '../../assets/top-angle-arrow-bold-icon.svg'
 
 function Vote(props) {
-    const [likeButton, setLikeButton] = useState(false)
-    const [disLikeButton, setDisLikeButton] = useState(false)
     const [error, setError] = useState(false)
-    const [disableLikeButton, setDisableLikeButton] = useState(false)
-    const [disableDisLikeButton, setDisableDisLikeButton] = useState(false)
 
     function VoteHandlerApi(voteCount) {
-        if (voteCount === 1) {
-            setLikeButton((prevState) => !prevState)
-            setDisableDisLikeButton(true)
-            if (likeButton) {
-                setDisableDisLikeButton(false)
-                patchVoteById(props.article.article_id, -1).catch(() => {
-                    setError(true)
-                })
-                return props.setVotes((prevVote) => prevVote - 1)
-            }
-
-            patchVoteById(props.article.article_id, voteCount).catch(() => {
+        if (!voteCount) {
+            patchVoteById(props.article.article_id, 1).catch(() => {
                 setError(true)
             })
             props.setVotes((prevVote) => prevVote + 1)
         } else {
-            setDisLikeButton((prevState) => !prevState)
-            setDisableLikeButton(true)
-            if (disLikeButton) {
-                setDisableLikeButton(false)
-                patchVoteById(props.article.article_id, 1).catch(() => {
-                    setError(true)
-                })
-                return props.setVotes((prevVote) => prevVote + 1)
-            }
-
-            patchVoteById(props.article.article_id, voteCount).catch(() => {
+            patchVoteById(props.article.article_id, -1).catch(() => {
                 setError(true)
             })
             props.setVotes((prevVote) => prevVote - 1)
@@ -47,38 +22,22 @@ function Vote(props) {
     }
 
     return (
-        <div className={styles.vote_div}>
-            <div className={styles.vote}>
-                <b>CLICK TO VOTE :</b>
+        <div className={styles.vote}>
+            <img
+                onClick={() => VoteHandlerApi(true)}
+                className={styles.like}
+                alt="like"
+                src={upVote}
+            />
 
-                <Button
-                    onClick={() => VoteHandlerApi(+1)}
-                    disabled={disableLikeButton}
-                    className={
-                        likeButton
-                            ? styles.nc_button_selected
-                            : styles.nc_button
-                    }
-                >
-                    <img className={styles.like} alt="like" src={likeicon} />
-                </Button>
-                <Button
-                    onClick={() => VoteHandlerApi(-1)}
-                    disabled={disableDisLikeButton}
-                    className={
-                        disLikeButton
-                            ? styles.nc_button_selected
-                            : styles.nc_button
-                    }
-                >
-                    <img
-                        alt="unlike"
-                        className={styles.unlike}
-                        src={dislikeicon}
-                    />
-                </Button>
-            </div>
-            {error ? <p> Could not Vote try again ... </p> : null}
+            <img
+                onClick={() => VoteHandlerApi(false)}
+                alt="unlike"
+                className={styles.unlike}
+                src={downVote}
+            />
+
+            {error ? <p> Could not vote try again ... </p> : null}
         </div>
     )
 }
